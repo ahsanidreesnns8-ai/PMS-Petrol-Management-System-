196a4d8ad1d2af9f90061501371abf9e6ae12158
ahsanidreesnns8 <ahsanidreesnns8@gmail.com>
2026-07-16 21:12:50 +0500

    change swagger.js after successfully deployed

--- backend/config/swagger.js
+++ backend/config/swagger.js
@@ -14,8 +14,8 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
-        description: 'Development server',
+        url: "https://pms-petrol-management-system-production.up.railway.app",
+        description: "Production Server",
       },
     ],
     components: {
